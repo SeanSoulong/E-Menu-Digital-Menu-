32e543c86bdd762f3f6359a63478e67db3bbc073
thesou168 <sean.soulong.2822@rupp.edu.kh>
2026-01-18 23:12:59 +0700

    Initial commit

--- app/context/LanguageContext.tsx
+++ app/context/LanguageContext.tsx
@@ -13,7 +13,7 @@ const LanguageContext = createContext<LanguageContextType | undefined>(
 );
 
 export const LanguageProvider = ({ children }: { children: ReactNode }) => {
-  const [language, setLanguage] = useState<Language>("en");
+  const [language, setLanguage] = useState<Language>("kh");
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
       {children}
